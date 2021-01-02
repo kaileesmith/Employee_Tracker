9f3e0c9534b9e2aa0defa4383d074568d8188e08
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-02 15:51:21 -0800

    updated schema.sql to include 3 tables

--- schema.sql
+++ schema.sql
@@ -8,3 +8,23 @@ CREATE TABLE department (
   name varchar(30) NOT NULL,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title varchar(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+ FOREIGN KEY(role_id) REFERENCES role(id),
+ FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
